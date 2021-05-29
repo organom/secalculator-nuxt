85ed4b18b2edd538049fe210f29b2ad12f6c2be8
organom <organom@users.noreply.github.com>
2021-05-29 17:45:04 +0000

    Deploying to gh-pages from @ organom/secalculator@80d4357128867c09e53ba7801c0dd3571712ba2b 🚀

---
+++ _nuxt/static/1622310302/components/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsecalculator\u002F_nuxt\u002Fstatic\u002F1622310302",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcomponents",config:{_app:{basePath:"\u002Fsecalculator\u002F",assetsPath:"\u002Fsecalculator\u002F_nuxt\u002F",cdnURL:a}}}}(null));
