a98b66a2d943610109896aaa04db4a03a55a4b61
Ricardo Gomes <ricardo@heptasoft.com>
2021-05-31 08:06:14 +0100

    Renamed load_base to se_helper. Use method code instead of UI

--- pages/index.vue
+++ pages/index.vue
@@ -19,34 +19,16 @@
 </template>
 
 <script>
-const fastParser = require('fast-xml-parser');
+const sehelper = require('assets/se_helper');
 
 export default {
   name: 'index',
   async asyncData({ $http, store }) {
     // Base Blocks
     if(store.state.base.blocks.length === 0 ) {
-      const files = ["CubeBlocks", "CubeBlocks_Armor", "CubeBlocks_Armor_2", "CubeBlocks_Automation", "CubeBlocks_Communications", "CubeBlocks_Control", "CubeBlocks_DecorativePack",
-        "CubeBlocks_DecorativePack2", "CubeBlocks_Doors", "CubeBlocks_Economy", "CubeBlocks_Energy", "CubeBlocks_Extras", "CubeBlocks_Frostbite", "CubeBlocks_Gravity", "CubeBlocks_Interiors",
-        "CubeBlocks_LCDPanels", "CubeBlocks_Lights", "CubeBlocks_Logistics", "CubeBlocks_Mechanical", "CubeBlocks_Medical", "CubeBlocks_Production", "CubeBlocks_ScrapRacePack", "CubeBlocks_SparksOfTheFuturePack",
-        "CubeBlocks_Symbols", "CubeBlocks_Thrusters", "CubeBlocks_Tools", "CubeBlocks_Utility", "CubeBlocks_Warfare1", "CubeBlocks_Weapons", "CubeBlocks_Wheels", "CubeBlocks_Windows"]
-
-      const blocks = [];
-      for (let i = 0; i < files.length; i++) {
-        const response = await $http.$get(`https://organom.github.io/secalculator/CubeBlocks/${files[i]}.sbc`)
-        const jsonObj = await fastParser.parse(response, {
-          attributeNamePrefix: "@_",
-          ignoreAttributes: false,
-          ignoreNameSpace: false
-        });
-        if (jsonObj.Definitions) {
-          blocks.push(jsonObj.Definitions.CubeBlocks.Definition);
-        }
-      }
-      const flatten = blocks.flat();
-      store.commit('base/setBlocks', flatten);
-      return {}
+      store.commit('base/setBlocks', await sehelper.loadBaseBlocks());
     }
+    return {}
   },
   computed: {
     baseBlocks () {
